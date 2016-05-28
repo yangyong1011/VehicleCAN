9817be0eb0965f067639a0c642bb46d4d5c1b255
yangyong1011 <272591929@qq.com>
2016-05-28 15:53:05 +0800

    add types.h

---
+++ VehicleCAN_for_STM32/source/lib/types.h
@@ -0,0 +1,14 @@
+#ifndef _TYPES_H
+#define _TYPES_H
+        typedef unsigned char       T_bit;
+        typedef unsigned char       T_u8;
+        typedef signed   char       T_s8;
+        typedef char                T_char;
+        typedef unsigned short      T_u16;
+        typedef unsigned long       T_u32;
+        typedef unsigned long long  T_u64;
+        typedef signed short        T_s16;
+        typedef signed long         T_s32;
+        typedef signed long long    T_s64;
+        typedef float               T_f32;
+#endif
